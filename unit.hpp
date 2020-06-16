db039377e28240b5b3c53e97f4d31639adff7a8c
M-A-abeer-Sohail <60892508+M-A-abeer-Sohail@users.noreply.github.com>
2020-06-16 15:27:11 +0500

    Added first files

--- unit.hpp
+++ unit.hpp
@@ -1,3 +1,15 @@
+#include "SDL.h"
+#include <cstdio>
+
 class Unit
 {
+protected:
+    SDL_Texture *assets;
+    SDL_Rect src;
+    SDL_Rect mover;
+
+public:
+    Unit(SDL_Texture *);
+    virtual void draw(SDL_Renderer *); // draws the child class object onto the SDL surface
+    SDL_Rect getMover();
 };
